916162d3938cd1cf2b2e0f4f125db8fa4aced8aa
Mahfuj Ahbab <ahbab@umich.edu>
2022-01-24 11:30:33 -0500

    cleaned up package interface

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='stream',
     version='0.1.0',
-    packages=find_packages(include=['app', 'app.*']),
+    packages=find_packages(include=['stream', 'stream.*']),
     install_requires=[
         'streamlit==1.1.0',
         'bokeh==2.2.3'
--- stream/core.py
+++ stream/core.py
@@ -1,112 +1,125 @@
 from typing import List
 import streamlit as st
-class StreamlitPage:
+import pickle
+from collections import defaultdict
+class Page:
 
-    def __init__(self, name=None) -> None:
+    def __init__(self, name=None, group_name=None) -> None:
         self._name = name or self.__class__.__name__
-        self._group = None
+        self._group_name = group_name or 'DefaultGroup'
 
     @property
     def name(self):
         return self._name
 
     @property
-    def locals(self):
-        if self._group is not None:
-            return self._group.variables
-        else:
-            raise Exception('Page must be part of a group to access local variables')
+    def group(self):
+        return self._group_name
+
+    @property
+    def variables(self):
+        return st.session_state.variables
+
+    @property
+    def single_targets(self):
+        return st.session_state.single_target
+
+    @property
+    def multi_targets(self):
+        return st.session_state.multi_target
 
     @property
     def globals(self):
         return st.session_state.globals
 
-    def get_variable(self, key: str, valid_types: tuple = None):
-        if key in self.locals:
-            var = self.locals[key]
-        elif key in self.globals:
-            var = self.globals[key]
-        else:
-            st.error(
-                "The variable '{}' was not defined for page '{}'".format(key, self.name)
-            )
-        
-        if valid_types is not None:
-            if isinstance(var, valid_types):
-                return var
-            else:
-                st.error(
-                    "Variable '{}' of type '{}' is an invalid type".format(key, var.__class__.__name__)
-                )
-
+    def set_variable(self, key: str, value):
+        st.session_state.variables[value.__class__.__name__][key] = value
 
     def __call__(self):
         raise NotImplementedError()
 
-class StreamlitPageGroup:
 
-    def __init__(self, name, input_func, *pages):
-        self._name = name
-        self._input_func = input_func
-        self._pages = {p.name: p for p in pages}
-        for p_name in self._pages:
-            self._pages[p_name]._group = self
 
-    @property
-    def name(self):
-        return self._name
+class App:
 
-    @property
-    def variables(self):
-        return st.session_state.locals[self.name]
-
-    @property
-    def pages(self):
-        return self._pages
-
-    @property
-    def inputs(self):
-        return self._input_func
-
-class StreamlitApp:
-
-    FIRST_RUN = True
-
-    def __init__(self, page_groups: List[StreamlitPageGroup], **global_vars) -> None:
-        self._page_groups = {p.name: p for p in page_groups}
+    def __init__(self, pages: List[Page], **global_vars) -> None:
         self._global_vars = global_vars
+        self._page_groups = defaultdict(dict)
+        self._page_groups['Main Menu']['Selections'] = self.target_page
+        for p in pages:
+            self._page_groups[p.group][p.name] = p
 
-    @property
-    def inputs(self):
-        return {p: self._page_groups[p].inputs for p in self._page_groups}
 
-    def inputs_page(self):
-        local_vars = {}
-        st.header('Inputs Page')
-        for p in self._page_groups:
-            st.subheader('{} Inputs'.format(p.name))
-            local_vars[p.name] = p.inputs()
-        submit = st.form_submit_button('Submit', on_click=lambda x: st.session_state.locals.update(x), kwargs={'x': local_vars})
-    
-    def run(self):
-        #intialize global variables
-        st.session_state.globals = self._global_vars
-        if 'locals' not in st.session_state:
-            st.session_state.locals = {}
+    def setup(self):
 
-        if self.FIRST_RUN:
-            self.FIRST_RUN = False
+        if 'variables' not in st.session_state:
+            st.session_state.variables = defaultdict(dict)
 
-        #page selection options
+        if 'single_target' not in st.session_state:
+            st.session_state.single_target = defaultdict(lambda: None)
+
+        if 'multi_target' not in st.session_state:
+            st.session_state.multi_target = defaultdict(list)
+
+        if 'globals' not in st.session_state:
+            st.session_state.globals = self._global_vars
+            st.session_state.globals['current_page'] = self.target_page
+
+    def _update_variable(self, key, choices, multi=False):
+        if not multi:
+            obj_key = st.session_state['single_target_{}'.format(key)]
+            st.session_state.single_target[key] = choices[obj_key]
+        else:
+            obj_keys = st.session_state['multi_target_{}'.format(key)]
+            st.session_state.multi_target[key] = [choices[x] for x in obj_keys]
+        st.success('Variables sucessfully updated!')
+
+    def _update_current_page(self, page: Page):
+        st.session_state.globals['current_page'] = page
+    def target_page(self):
+
+        st.header('Selections')
+
+        with st.expander('Current Targets', True):
+            st.markdown('Single Targets')
+            st.write(st.session_state.single_target)
+
+            st.markdown('Multi Targets')
+            st.write(st.session_state.multi_target)
+
+
+        for key in st.session_state.variables:
+            choices = st.session_state.variables[key]
+            new_target_key = st.selectbox(
+                label='Single Target Select: {}'.format(key), 
+                options=choices.keys(), 
+                key='single_target_{}'.format(key),
+                on_change=self._update_variable,
+                kwargs={'key': key, 'choices': choices, 'multi': False}
+            )
+            new_target_keys = st.multiselect(
+                label='Multi Target Select: {}'.format(key), 
+                options=choices.keys(), 
+                key='multi_target_{}'.format(key),
+                on_change=self._update_variable,
+                kwargs={'key': key, 'choices': choices, 'multi': True}
+            )
+
+    def sidebar_options(self):
         page_group_selection = st.sidebar.radio('Select Page Group', self._page_groups.keys())
-        page_options = self._page_groups[page_group_selection].pages
+        page_options = self._page_groups[page_group_selection]
         page_name = st.sidebar.radio('Select Page', page_options.keys())
         page_selection = page_options[page_name]
+        self._update_current_page(page_selection)
+    
+    def run(self):
+        #initialize session state
+        self.setup()
+
+        #page selection options
+        self.sidebar_options()
 
         #either display selected page or inputs
-        if st.sidebar.button('Go to Inputs') or self.FIRST_RUN:
-            self.FIRST_RUN = False
-            with st.form('input_page'):
-                self.inputs_page()
-        else:
-            page_selection() 
+        current_page = st.session_state.globals['current_page']
+        current_page()
+
