5c958b3d40373d6966b1dd94dbd591faad46f153
Mahfuj Ahbab <mahfuj.ahbab@talentei.com>
2022-01-26 19:53:59 -0500

    clean App object

--- stream/core.py
+++ stream/core.py
@@ -1,4 +1,3 @@
-from ssl import OP_SINGLE_ECDH_USE
 from typing import List
 import streamlit as st
 import pickle
@@ -6,10 +5,10 @@ from collections import defaultdict
 
 class State:
 
-    def setup(refresh=False):
+    def setup(starting_page=None, refresh=False):
 
         if ('globals' not in st.session_state) or (refresh):
-            st.session_state.globals = State.clear_globals()
+            st.session_state.globals = State.clear_globals(starting_page)
         if('locals' not in st.session_state) or (refresh):
             st.session_state.locals = defaultdict(dict)
 
@@ -25,6 +24,9 @@ class State:
     def locals():
         return st.session_state.locals
 
+    def update_current_page(page):
+        st.session_state.globals['current_page'] = page
+
     def update_local_variables(group_name, **new_vals):
         for key in new_vals:
             st.session_state.locals[group_name][key] = new_vals[key]
@@ -38,9 +40,9 @@ class State:
             obj_type = obj.__class__.__name__
             st.session_state.globals['variables'][obj_type][key] = obj
 
-    def clear_globals():
+    def clear_globals(starting_page=None):
         globals_dict = {
-            'current_page': State.target_page,
+            'current_page':starting_page or State.target_page,
             'selections': defaultdict(list),
             'variables': defaultdict(dict),
         }
@@ -74,14 +76,14 @@ class State:
             st.write(State.selections)
 
 
-        for key in State.variables:
-            choices = State.variables[key]
+        for key in State.variables():
+            choices = State.variables()[key]
             widg_key = 'stream_selection_{}'.format(key)
 
             new_target_keys = st.multiselect(
                 label='Selection: {}'.format(key), 
                 options=choices.keys(), 
-                default=State.selections[key],
+                default=State.selections()[key],
                 key=widg_key,
                 on_change=State.update_selection,
                 kwargs={'dtype': key},
@@ -94,6 +96,35 @@ class State:
             )
 
             st.button(
+                label='Delete All Variables',
+                on_click=State.clear_globals
+            )
+
+    def target_page_sidebar():
+
+        st.subheader('Selections')
+
+        with st.expander('target_page_sidebar'):
+            for key in State.variables():
+                choices = State.variables()[key]
+                widg_key = 'stream_selection_{}'.format(key)
+
+                new_target_keys = st.sidebar.multiselect(
+                    label='Selection: {}'.format(key), 
+                    options=choices.keys(), 
+                    default=State.selections()[key],
+                    key=widg_key,
+                    on_change=State.update_selection,
+                    kwargs={'dtype': key},
+                    args=(x for x in st.session_state[widg_key])
+                )
+
+            st.sidebar.button(
+                label='Clear All Selections',
+                on_click=State.clear_selection
+            )
+
+            st.sidebar.button(
                 label='Delete All Variables',
                 on_click=State.clear_globals
             )
@@ -136,73 +167,15 @@ class App:
     def __init__(self, pages: List[Page], **global_vars) -> None:
         self._global_vars = global_vars
         self._page_groups = defaultdict(dict)
-        self._page_groups['Main Menu']['Selections'] = self.target_page
         for p in pages:
             self._page_groups[p.group][p.name] = p
-
-
-    def setup(self):
-
-        if 'variables' not in st.session_state:
-            st.session_state.variables = defaultdict(dict)
-
-        if 'single_target' not in st.session_state:
-            st.session_state.single_target = defaultdict(lambda: None)
-
-        if 'multi_target' not in st.session_state:
-            st.session_state.multi_target = defaultdict(list)
-
-        if 'globals' not in st.session_state:
-            st.session_state.globals = self._global_vars
-            st.session_state.globals['current_page'] = self.target_page
-
-    def _update_variable(self, key, choices, multi=False):
-        if not multi:
-            obj_key = st.session_state['single_target_{}'.format(key)]
-            st.session_state.single_target[key] = choices[obj_key]
-        else:
-            obj_keys = st.session_state['multi_target_{}'.format(key)]
-            st.session_state.multi_target[key] = [choices[x] for x in obj_keys]
-        st.success('Variables sucessfully updated!')
-
-    def _update_current_page(self, page: Page):
-        st.session_state.globals['current_page'] = page
-        
-    def target_page(self):
-
-        st.header('Selections')
-
-        with st.expander('Current Targets', True):
-            st.markdown('Single Targets')
-            st.write(st.session_state.single_target)
-
-            st.markdown('Multi Targets')
-            st.write(st.session_state.multi_target)
-
-
-        for key in st.session_state.variables:
-            choices = st.session_state.variables[key]
-            new_target_key = st.selectbox(
-                label='Single Target Select: {}'.format(key), 
-                options=choices.keys(), 
-                key='single_target_{}'.format(key),
-                on_change=self._update_variable,
-                kwargs={'key': key, 'choices': choices, 'multi': False}
-            )
-            new_target_keys = st.multiselect(
-                label='Multi Target Select: {}'.format(key), 
-                options=choices.keys(), 
-                key='multi_target_{}'.format(key),
-                on_change=self._update_variable,
-                kwargs={'key': key, 'choices': choices, 'multi': True}
-            )
 
     def sidebar_options(self):
         page_group_selection = st.sidebar.radio('Select Page Group', self._page_groups.keys())
         page_options = self._page_groups[page_group_selection]
         page_name = st.sidebar.radio('Select Page', page_options.keys())
         page_selection = page_options[page_name]
-        self._update_current_page(page_selection)
+        State.update_current_page(page_selection)
     
     def run(self):
         #initialize session state
@@ -210,9 +183,9 @@ class App:
 
         #page selection options
         self.sidebar_options()
+        State.target_page_sidebar()
 
         #either display selected page or inputs
-        current_page = st.session_state.globals['current_page']
-        current_page()
+        State.current_page()()
 
 
