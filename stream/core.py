9b6e7e7d6eaa3e80ac0118cb04eb7032d1d713d8
Mahfuj Ahbab <mahfuj.ahbab@talentei.com>
2022-02-01 12:12:51 -0500

    added selections page and plotting page base class

--- stream/core.py
+++ stream/core.py
@@ -2,6 +2,10 @@ from typing import List
 import pandas as pd
 import streamlit as st
 from collections import defaultdict
+from .plotting import HEIGHT, WIDTH, TOOLBAR_LOC, SIZING_MODE
+import bokeh.models as bkm
+import bokeh.plotting as bkp
+import numpy as np
 
 class State:
 
@@ -73,12 +77,11 @@ class State:
         else:
             st.session_state.globals['selections'][dtype_key].clear()
 
-      
 class Page:
 
     def __init__(self, name=None, group_name=None, is_setup_page=False) -> None:
         self._name = name or self.__class__.__name__
-        self._group_name = group_name or 'DefaultGroup'
+        self._group_name = group_name or 'Default Group'
         self._is_setup_page = is_setup_page
 
     @property
@@ -106,10 +109,162 @@ class Page:
     def __call__(self):
         raise NotImplementedError()
 
+class Selection(Page):
+
+    '''
+    Page to select variables which can be accessed from calling
+    :func: State.variables <stream.core.State.variables>
+    '''
+
+    def __call__(self):
+
+        with st.expander('Current Selections'):
+            st.write(State.selections)
+
+        with st.expander('Local Variables'):
+            local_vars = State.locals()
+            for key in local_vars:
+                group_vars = local_vars[key]
+                if not group_vars:
+                    continue
+                st.subheader(key)
+                st.write(group_vars)
+
+        for key in State.variables():
+            choices = State.variables()[key]
+            widg_key = 'stream_selection_{}'.format(key)
+
+            st.multiselect(
+                label='Selection: {}'.format(key), 
+                options=choices.keys(), 
+                default=State.selections()[key],
+                key=widg_key,
+                on_change=State.update_selection,
+                kwargs={'dtype': key},
+                args=(x for x in st.session_state[widg_key])
+            )
+
+        st.button(
+            label='Clear All Selections',
+            on_click=State.clear_selection
+        )
+
+        st.button(
+            label='Delete All Variables',
+            on_click=State.reset_globals
+        )
+
+class PlottingPage(Page):
+
+    '''
+    :class: Page <stream.core.Page> object with helper functions to visualize DataFrames. 
+    There are two ways to utilize this object:
+        1) Pass it as a page to :class: App <stream.core.App> without specifying :arg: target_variable.
+        PlottingPage will allow the user to choose a DataFrame from the selections under globals.
+        2) Pass it as a page to :class: App <stream.core.App> while specifying :arg: target_variable.
+        PlottingPage will look for the DataFrame with name :arg: target_variable under local variables.
+    '''
+
+    def __init__(
+        self,
+        target_variable=None,
+        name=None,
+        group_name=None
+    ) -> None:
+        super().__init__(name, group_name)   
+        self._target_variable = target_variable
+
+    def _default_figure(self, datetime_x=False, ttips=None):
+        fig_kwargs = {
+            'height': HEIGHT,
+            'width': WIDTH,
+            'toolbar_location': TOOLBAR_LOC,
+            'sizing_mode': SIZING_MODE
+        }
+        if datetime_x:
+            fig_kwargs['x_axis_type'] = 'datetime'
+        fig = bkp.figure(**fig_kwargs)
+        if ttips is not None:
+            hover = bkm.HoverTool(tooltips=ttips, mode='vline')
+            fig.add_tools(hover)
+        
+        return fig
+
+    def _figure_format(self, figure, xlabel=None, ylabel=None, title=None):
+        figure.legend.location = 'top_left'
+        figure.legend.click_policy = 'hide'
+
+        if xlabel is not None:
+            figure.xaxis.axis_label = xlabel
+        if ylabel is not None:
+            figure.yaxis.axis_label = ylabel
+        figure.axis.axis_label_text_font_size = '12pt'
+        
+        if title is not None:
+            figure.title.text = title
+            figure.title.text_font_size = '16pt'
+        return figure
+
+    def _bquad(self, srs, bins):
+        bounds_dict = {}
+        try:
+            rbins = pd.cut(srs, np.linspace(srs.min(), srs.max(), bins))
+        except ValueError:
+            st.error('Error generating bins. Try decrease the bin count or setting a longer timeframe')
+        rgrp = rbins.groupby(rbins).count()
+        bounds_dict['top'] = [x for x in rgrp]
+        bounds_dict['left'] = [x.left for x in rgrp.index]
+        bounds_dict['right'] = [x.right for x in rgrp.index]
+        bounds_dict['bottom'] = 0
+        return bounds_dict
+
+    def hist(self, srs: pd.Series, bins: int, fig=None, color=None):
+
+        if not fig:
+            ttips = [('Name', '$name'), ('Count', '@top'), ('Value', '$x{0,0.00}')]
+            fig = self._default_figure(ttips=ttips)
+
+        kw = self._bquad(srs, bins)
+        kw['legend_label'] = kw['name'] = srs.name
+        if color is not None:
+            kw['color'] = color
+        fig.quad(**kw)
+        return fig
+
+    def line(self, source, y_col, x_col='Date', fig=None, color=None, is_time_series=True):
+
+        #pre process data
+
+        if fig is None:
+            ttips = [('Name', '@{}'.format(y_col)), ('Value', '@value{0.00a}')]
+            if is_time_series:
+                ttips.insert(1, ('Date', '@date_str'))
+            else:
+                ttips.insert(1, ('Index', '@{}'.format(x_col)))
+
+            fig = self._default_figure(is_time_series, ttips)
+
+        #in case dframe has multi-index columns
+
+        plt_kwargs = {
+            'y': y_col,
+            'x': x_col,
+            'source': source,
+            'legend_label': y_col, 
+        }
+
+        if color is not None:
+            plt_kwargs['color'] = color
+
+        fig.line(**plt_kwargs)
+
+        return fig
 
 class App:
 
     def __init__(self, pages: List[Page], setup_on_every_run=False, display_local_variables=False, **global_vars) -> None:
+        if Selection.__name__ not in [p.__class__.__name__ for p in pages]:
+            pages.append(Selection(group_name='Main Menu'))
         self._global_vars = global_vars
         self._pages = pages
         self._page_groups = defaultdict(dict)
@@ -119,13 +274,18 @@ class App:
         for p in pages:
             self._page_groups[p.group][p.name] = p
 
+    def _re_init(self):
+        st.session_state.globals['is_first_run'] = True
+
     def sidebar_options(self):
         page_group_selection = st.sidebar.radio('Select Page Group', self._page_groups.keys())
         page_options = self._page_groups[page_group_selection]
         display_page_choices = [p for p in page_options if not page_options[p]._is_setup_page]
         page_name = st.sidebar.radio('Select Page', display_page_choices)
         page_selection = page_options[page_name]
+        st.sidebar.button('Re-initialize', on_click=self._re_init)
         State.update_current_page(page_selection)
+
 
     def display_local_vars(self, page):
         with st.expander('Local Variables'):
@@ -168,63 +328,10 @@ class App:
         if self._display_local_variables:
             self.display_local_vars(current_page)
 
-        #either display selected page or inputs
         current_page()
 
-class Selection(Page):
-
-    def __call__():
-
-        with st.expander('Current Selections', True):
-            st.write(State.selections)
 
 
-        for key in State.variables():
-            choices = State.variables()[key]
-            widg_key = 'stream_selection_{}'.format(key)
-
-            st.multiselect(
-                label='Selection: {}'.format(key), 
-                options=choices.keys(), 
-                default=State.selections()[key],
-                key=widg_key,
-                on_change=State.update_selection,
-                kwargs={'dtype': key},
-                args=(x for x in st.session_state[widg_key])
-            )
-
-            st.button(
-                label='Clear All Selections',
-                on_click=State.clear_selection
-            )
-
-            st.button(
-                label='Delete All Variables',
-                on_click=State.reset_globals
-            )
-
-class PandasViewer(Page):
-
-    def __init__(
-        self,
-        target_variable=None,
-        name=None,
-        table=True,
-        group_name=None,
-        description=True,
-
-    ) -> None:
-        super().__init__(name, group_name)   
-        self._target_variable = target_variable
-
-    def __call__(self, dataframe=None):
-        if dataframe is None:
-            if self._target_variable is None:
-                options = self.selections[pd.DataFrame.__name__]
-                df_name = st.selectbox('DataFrames', options.keys())
-                dataframe = options[df_name]
-            else:
-                dataframe = self.locals[self._target_variable]
         
 
 
